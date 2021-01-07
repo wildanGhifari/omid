429145b9df101620447b3ac721aec034a6006eea
wildanGhifari <wildghifari@gmail.com>
2021-01-07 11:01:13 +0700

    Memrubah tampilan dan menambahkan jumbotron pada halaman about/index.php

--- application/views/pages/about/index.php
+++ application/views/pages/about/index.php
@@ -1,3 +1,11 @@
+<div class="jumbotron jumbotron-fluid" style="padding: 10% 0;">
+    <div class="container text-center text-white" style="margin-top:auto;" style="background-color: #fff;">
+        <h1>About Us</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, accusamus mollitia! In illum accusamus ex?</p>
+    </div>
+    <!-- <span style="margin-left:20px; margin-top:30px">Photo by <a href="https://unsplash.com/@maddibazzocco?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Maddi Bazzocco</a> on <a href="https://unsplash.com/s/photos/nuts?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span> -->
+</div>
+
 <main role="main">
     <section class="marginTop">
         <div class="container-xl">
@@ -30,6 +38,22 @@
         </div>
     </section>
 
+    <section>
+        <div class="container-xl" style="padding: 5% 10px;">
+            <div id="aboutOmidRow">
+                <div class="aboutOmidCol">
+                    <img style="width: 100%; height: 100%;" src="<?= base_url('assets/img/eating_salad_.svg') ?>" alt="eating_salad">
+                </div>
+
+                <div class="aboutOmidCol" id="signupAbout">
+                    <h3>Sign up now! it's Free!</h3>
+                    <p class="mt-3">Sign up now and <strong>Get a 10% Off</strong> on your first purchase. You will also get promos and other interesting insights about healthy food.</p>
+                    <a style="letter-spacing: 0.1em;" class="btn btn-success rounded-pill text-uppercase mt-4" href="<?= base_url('/register') ?>">Sign up Now!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section class="marginTop">
         <div class="container-fluid bg-white" style="padding: 5% 0;">
             <div class="container-xl">
@@ -50,22 +74,6 @@
                 </div>
                 <div class="row justify-content-center py-4 px-3">
                     <a style="letter-spacing: 0.1em;" class="btn btn-success btn-lg rounded-pill text-uppercase mt-4" href="<?= base_url('shopping') ?>">See All Products</a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section>
-        <div class="container-xl" style="padding: 5% 10px;">
-            <div id="aboutOmidRow">
-                <div class="aboutOmidCol">
-                    <img style="width: 100%; height: 100%;" src="<?= base_url('assets/img/eating_salad_.svg') ?>" alt="eating_salad">
-                </div>
-
-                <div class="aboutOmidCol" id="signupAbout">
-                    <h3>Sign up now! it's Free!</h3>
-                    <p class="mt-3">Sign up now and <strong>Get a 10% Off</strong> on your first purchase. You will also get promos and other interesting insights about healthy food.</p>
-                    <a style="letter-spacing: 0.1em;" class="btn btn-success rounded-pill text-uppercase mt-4" href="<?= base_url('/register') ?>">Sign up Now!</a>
                 </div>
             </div>
         </div>

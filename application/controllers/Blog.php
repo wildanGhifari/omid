752376a2375cc3af4e1a80061ef464c2a22bb5ab
wildanGhifari <wildghifari@gmail.com>
2021-01-08 13:14:10 +0700

    Menambahkan link di footer untuk halaman blog dan Membuat beberapa function pada Model_blog.php dan Controller Blog.php

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -5,10 +5,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Blog extends MY_Controller
 {
 
-    public function index()
+    public function index($page = null)
     {
-        $data['title']  = 'Blog | Omid Health Style';
-        $data['page']   = 'pages/blog/index';
+        $data['title']      = 'Blog | Omid Health Style';
+        $data['contnet']    = $this->blog->select([
+            'blog.id', 'blog.slug', 'blog.title AS blog_title', 'blog.description',
+            'blog.image', 'blog_category.title AS blog_category_title', 'blog_category.slug AS blog_category_slug'
+        ])
+            ->join('blog_category')
+            ->paginate($page)
+            ->get();
+        $data['total_rows'] = $this->blog->count();
+        $data['pagination'] = $this->blog->makePagination(
+            base_url('blog'),
+            2,
+            $data['total_rows']
+        );
+        $data['page']       = 'pages/blog/index';
 
         $this->view($data);
     }
--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -6,6 +6,75 @@ class Blog_model extends MY_Model
 {
 
     protected $perPage = 20;
+
+    public function getDefaultValues()
+    {
+        return [
+            'id_blog_category'  => '',
+            'slug'              => '',
+            'title'             => '',
+            'description'       => '',
+            'image'             => ''
+        ];
+    }
+
+    public function getValidationRules()
+    {
+        $validationRules = [
+            [
+                'field' => 'id_blog_category',
+                'label' => 'Category',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'slug',
+                'label' => 'Slug',
+                'rules' => 'trim|required|callback_unique_slug'
+            ],
+            [
+                'field' => 'title',
+                'label' => 'Blog Name',
+                'rules' => 'trim|required'
+            ],
+            [
+                'field' => 'description',
+                'label' => 'Description',
+                'rules' => 'trim|required'
+            ]
+        ];
+
+        return $validationRules;
+    }
+
+    public function uploadImage($fieldName, $fileName)
+    {
+        $config = [
+            'upload_path'       => './images/blog',
+            'file_name'         => $fileName,
+            'allowed_types'     => 'jpg|gif|png|jpeg|JPG|PNG',
+            'max_size'          => 1024,
+            'max_width'         => 0,
+            'max_height'        => 0,
+            'overwrite'         => true,
+            'file_ext_tolower'  => true
+        ];
+
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload($fieldName)) {
+            return $this->upload->data();
+        } else {
+            $this->session->set_flashdata('image_error', $this->upload->display_errors('', ''));
+            return false;
+        }
+    }
+
+    public function deleteImage($fileName)
+    {
+        if (file_exists("./image/blog/$fileName")) {
+            unlink("./image/blog/$fileName");
+        }
+    }
 }
 
 /* End of file Blog_model.php */

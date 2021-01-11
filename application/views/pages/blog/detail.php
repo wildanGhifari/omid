b5dffc006bc672c5dd07bc433f8dd81f05fc7a69
wildanGhifari <wildghifari@gmail.com>
2021-01-11 17:07:34 +0700

    Memperbaiki halaman blog/detail.php

--- application/views/pages/blog/detail.php
+++ application/views/pages/blog/detail.php
@@ -4,8 +4,8 @@
             <div id="blog-detail" class="col-md-9">
                 <?php foreach ($blog as $row) : ?>
                     <h4><?= $row->blog_title ?></h4>
-                    <img style="width: 100%;" src="<?= $row->image ? base_url("images/blog/$row->image") : base_url("images/blog/default.jpg") ?>" alt="<?= $row->blog_title; ?>">
-                    <div class="container">
+                    <img src="<?= $row->image ? base_url("images/blog/$row->image") : base_url("images/blog/default.jpg") ?>" alt="<?= $row->blog_title; ?>">
+                    <div class="container-xl px-0">
                         <span><?= $row->description ?></span>
                         <hr>
                         <p><?= $row->content ?></p>

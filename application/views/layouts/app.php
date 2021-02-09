2cc1ec56ae1f54a8ad96c605aa2014bfac672959
wildanGhifari <wildghifari@gmail.com>
2021-02-09 14:51:16 +0700

    Initiate flickity

--- application/views/layouts/app.php
+++ application/views/layouts/app.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Supplier and online distributor of various kinds of healthy food products including nuts, dried fruit, seeds, grains, fresh fruits and vegetables.">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('/assets/libs/flickity/flickity.css') ?>">
 
     <!-- Bootstrap core CSS -->
     <link href="<?= base_url('/assets/libs/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
@@ -34,6 +35,7 @@
     <!-- End of Footer -->
     <script src="<?= base_url('/assets/libs/jquery/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('/assets/libs/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('/assets/libs/flickity/flickity.pkgd.min.js') ?>"></script>
     <script src="<?= base_url('/assets/js/app.js') ?>"></script>
     <script src="<?= base_url('/assets/js/ongkir.js') ?>"></script>
 

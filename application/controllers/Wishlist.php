6cbaf6e24be8c5d5ccacc1e0be6e571451920831
wildanGhifari <wildghifari@gmail.com>
2021-02-23 15:24:08 +0700

    Membuat method CRUD di controller wishlist dan menampilkan wishlist item

--- application/controllers/Wishlist.php
+++ application/controllers/Wishlist.php
@@ -25,7 +25,7 @@ class Wishlist extends MY_Controller
         $data['title']      = 'My Wishlist | Omid Health Style';
         $data['content']    = $this->wishlist->select([
             'wishlist.id', 'wishlist.qty', 'product.id', 'product.title',
-            'product.image', 'product.slug', 'product.category',
+            'product.image', 'product.slug', 'product.id_category',
             'product.price', 'product.weight'
         ])
             ->join('product')->where('wishlist.id_user', $this->id)->get();
@@ -33,6 +33,74 @@ class Wishlist extends MY_Controller
         $data['page']   = 'pages/wishlist/index';
 
         return $this->view($data);
+    }
+
+    public function add()
+    {
+        if (!$_POST || $this->input->post('qty') < 1) {
+            $this->session->set_flashdata('error', 'Product quantity cannot be empty.');
+            redirect(base_url());
+        } else {
+            $input                  = (object) $this->input->post(null, true);
+
+            $this->wishlist->table  = 'product';
+            $product                = $this->wishlist->where('id', $input->id_product)->first();
+
+            $subtotal               = $product->price * $input->qty;
+
+            $this->wishlist->table  = 'wishlist';
+            $wishlist                   = $this->wishlist->where('id_user', $this->id)->where('id_product', $input->id_product)->first();
+
+            if ($wishlist) {
+                $data = [
+                    'qty'           => $wishlist->qty + $input->qty,
+                    'subtotal'      => $wishlist->subtotal + $subtotal
+                ];
+
+                if ($this->wishlist->where('id', $wishlist->id)->update($data)) {
+                    $this->session->set_flashdata('success', 'Product successfully added!');
+                } else {
+                    $this->session->set_flashdata('error', 'Oops! Something went wrong.');
+                }
+
+                redirect(base_url(''));
+            }
+
+            $data = [
+                'id_user'       => $this->id,
+                'id_product'    => $input->id_product,
+                'qty'           => $input->qty,
+                'subtotal'      => $subtotal
+            ];
+
+            if ($this->wishlist->create($data)) {
+                $this->session->set_flashdata('success', 'Product successfully added!');
+            } else {
+                $this->session->set_flashdata('error', 'Oops! Something went wrong.');
+            }
+
+            redirect(base_url(''));
+        }
+    }
+
+    public function delete($id)
+    {
+        if (!$_POST) {
+            redirect(base_url('wishlist/index'));
+        }
+
+        if (!$this->wishlist->where('id', $id)->first()) {
+            $this->session->set_flashdata('warning', 'Sorry, The data does not exist!');
+            redirect(base_url('wishlist/index'));
+        }
+
+        if ($this->wishlist->where('id', $id)->delete()) {
+            $this->session->set_flashdata('success', 'Data successfully deleted!');
+        } else {
+            $this->session->set_flashdata('error', 'Oops! Something went wrong.');
+        }
+
+        redirect(base_url('wishlist/index'));
     }
 }
 
--- application/views/pages/shopping/index.php
+++ application/views/pages/shopping/index.php
@@ -36,13 +36,26 @@
                                     <small><a class="category text-uppercase" href="<?= base_url("/shop/category/$row->category_slug") ?>"><?= $row->category_title; ?></a></small>
                                     <p class="card-title"><?= $row->product_title; ?></p>
                                     <h5>Rp.<?= number_format($row->price, 0, ',', '.') ?></h5>
-                                    <form action="<?= base_url("/cart/add") ?>" method="POST" class="mt-4">
-                                        <input type="hidden" name="id_product" value="<?= $row->id ?>">
-                                        <div class="input-group">
-                                            <input class="form-control" type="hidden" name="qty" value="1" min="1">
-                                            <button type="submit" class="btn btn-success" style="width: 100%;">Add To Cart</button>
+                                    <div class="row mt-4">
+                                        <div class="col-md-8">
+                                            <form action="<?= base_url("/cart/add") ?>" method="POST">
+                                                <input type="hidden" name="id_product" value="<?= $row->id ?>">
+                                                <div class="input-group">
+                                                    <input class="form-control" type="hidden" name="qty" value="1" min="1">
+                                                    <button type="submit" class="btn btn-success" style="width: 100%;">Add To Cart</button>
+                                                </div>
+                                            </form>
                                         </div>
-                                    </form>
+                                        <div class="col-md-4">
+                                            <form action="<?= base_url("/wishlist/add") ?>" method="POST">
+                                                <input type="hidden" name="id_product" value="<?= $row->id ?>">
+                                                <div class="input-group">
+                                                    <input class="form-control" type="hidden" name="qty" value="1" min="1">
+                                                    <button type="submit" class="btn btn-outline-danger" style="width: 100%;"><i class="far fa-heart fa-lg"></i></button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>

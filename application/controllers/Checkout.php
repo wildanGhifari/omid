69a12708c1f99e8741a2a764cf394907426bd8c2
wildanGhifari <wildghifari@gmail.com>
2021-03-04 11:20:02 +0700

    Membuat fitur notifikasi ke admin OMID melalui email jika ada pesanan masuk

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -103,6 +103,7 @@ class Checkout extends MY_Controller
             'district'      => $city,
             'subdistrict'   => $subdistrict,
             'courier'       => $this->input->post('nama-ekspedisi'),
+            'weight'       => $this->input->post('total-berat'),
             'package'       => $service . '-' . $ongkir,
             'phone'         => $input->phone,
             'status'        => 'waiting'
@@ -120,6 +121,108 @@ class Checkout extends MY_Controller
             $this->db->delete('cart', ['id_user' => $this->id]);
 
             $this->session->set_flashdata('success', 'Data successfully saved.');
+
+            $this->checkout->table = 'user';
+            $data['user'] = $this->checkout->select([
+                'user.id', 'user.email'
+            ]) ->where('user.email', $this->id)->get();
+
+            if (isset($_POST['submit_email'])) {
+                $data = [
+                    'id_user'       => $this->id,
+                    'date'          => date('Y-m-d'),
+                    'invoice'       => $this->id . date('YmdHis'),
+                    'total'         => str_replace(".", "", $this->input->post('total')),
+                    'name'          => $input->name,
+                    'address'       => $input->address,
+                    'province'      => $province,
+                    'district'      => $city,
+                    'subdistrict'   => $subdistrict,
+                    'courier'       => $this->input->post('nama-ekspedisi'),
+                    'weight'       => $this->input->post('total-berat'),
+                    'package'       => $service . '-' . $ongkir,
+                    'phone'         => $input->phone,
+                    'status'        => 'waiting'
+                ];
+
+                if (!empty($data)) {
+                    
+                    // Konfigurasi to email proccess
+
+                    $config = [
+                        'mailtype' => 'html',
+                        'charset' => 'iso-8859-1',
+                        'protocol' => 'smtp',
+                        'smtp_host' => 'ssl://smtp.googlemail.com',
+                        'smtp_user' => 'omidhealth.kelapagading@gmail.com',
+                        'smtp_pass' => 'Nutsarehealthy123',
+                        'smtp_port' => 465,
+                        'newline' => "\r\n"
+                    ];
+
+                    $this->load->library('email', $config);
+                    $this->email->initialize($config);
+
+                    // $this->checkout->table = 'user';
+                    // $data['user'] = $this->checkout->select([
+                    //     'user.id', 'user.name', 'user.email', ''
+                    // ]) ->where('user.id', $this->id)->get();
+
+
+                    // ambil detail data pesanan
+                    $nama       = $data['name'];
+                    $provinsi   = $data['province'];
+                    $distrik    = $data['district'];
+                    $subdistrik = $data['subdistrict'];
+                    $paket      = $data['package'];
+                    $hp         = $data['phone'];
+                    $invoice    = $data['invoice'];
+                    $total      = $data['total'];
+                    $tanggal    = $data['date'];
+                    $kurir      = $data['courier'];
+                    $alamat     = $data['address'];
+
+
+                    $this->checkout->table  = 'orders';
+                    $data['order']          = $this->checkout->select([
+                        'orders.id', 'orders.id_user'
+                    ])  ->where('orders.id_user', $this->id)->get();
+
+                    $order = $data['order'][0]->id;
+
+                    $this->email->from('no-reply@omidhealthstyle.com');
+                    $this->email->to('wildghifari@gmail.com');
+                    $this->email->subject('New Order!, Ada pesanan baru dari ' . $nama . ' pada ' . $tanggal);
+                    $this->email->message('
+                    <div style="text-align: center; max-width: 450px; margin: 24px auto; border: solid 1px #acb5bd; border-radius: 15px; padding: 48px">
+                        <div style="width: 100%;">
+                            <div style="text-align: left;">
+                                <a href="https://omidhealthstyle.com/" style="margin-bottom: 24px;">
+                                    <img src="https://omidhealthstyle.com/assets/img/OmidLogo.png" alt="Logo Omid Health Style" height="68">
+                                </a>
+                                <div style="color: #212429 !important;">
+                                    <h3>Hai <strong>Omid Health Style,</strong></h3>
+                                    <h2 style="color: #06a954;">Ada pesanan baru sebesar Rp. ' . number_format($total, 0, ',', '.') . '</h2>
+                                    <hr style="margin: 24px 0;">
+                                    <p>No. Invoice: <strong><a style="color: #06a954;" href="' . base_url("/order/detail/$order") . '">' . $invoice . '</a></strong></p>
+                                    <p>Tanggal Pemesanan: ' . $tanggal . '</p>
+                                    <p>Kurir: ' . $kurir . '-' . $paket . '</p>
+                                    <p>Tujuan Pengiriman: <br> <strong>' . $nama .' (' . $hp . ')</strong><br>' .$alamat . ' ' . $provinsi .' ' .$distrik . ' ' . $subdistrik .'</p>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="' . base_url("/order/detail/$order") . '" style="width: 100%; text-decoration: none; font-weight: bold; letter-spacing: 2px; color: #fff;"><button style="background-color: #06a954; color: #fff; width: 100%; border: none; border-radius: 15px; margin-top: 32px; font-size: 1rem; padding: 16px;">Lihat Detail Pesanan</button></a>
+                    </div>
+                    ');
+
+                    if ($this->email->send()) {
+                        $this->session->set_flashdata('success', 'Data successfully saved.');
+                    } else {
+                        echo "Gagal";
+                        show_error($this->email->print_debugger());
+                    }
+                }
+            }
 
             $data['title']        = 'Checkout Success';
             $data['content']    = (object) $data;

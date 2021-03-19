cf98fe99b88f2857a2a28a56f8b0d1d93d111d5f
wildanGhifari <wildghifari@gmail.com>
2021-03-19 14:00:34 +0700

    Delete bootstrap 5 cdn, dan update tampilan halaman FAQs

--- application/views/layouts/app.php
+++ application/views/layouts/app.php
@@ -10,8 +10,6 @@
 
     <!-- Bootstrap 4 core CSS -->
     <link href="<?= base_url('/assets/libs/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <!-- Bootstrap 5 core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <!-- Fontawesome CSS -->
     <link rel="stylesheet" href="<?= base_url('/assets/libs/fontawesome/css/all.min.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -37,7 +35,6 @@
     <!-- End of Footer -->
     <script src="<?= base_url('/assets/libs/jquery/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('/assets/libs/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="<?= base_url('/assets/libs/flickity/flickity.pkgd.min.js') ?>"></script>
     <script src="<?= base_url('/assets/js/app.js') ?>"></script>
     <script src="<?= base_url('/assets/js/ongkir.js') ?>"></script>
--- application/views/pages/faqs/index.php
+++ application/views/pages/faqs/index.php
@@ -3,88 +3,104 @@
         <div class="row">
             <div class="col-md-10" style="margin:0 auto;">
                 <h3>Frequently asked questions</h3>
-                <div class="accordion mt-4" id="accordionExample">
-                    <div class="accordion-item">
-                        <div class="accordion-header" id="headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <h5>1. Dimana lokasi Omid Health Style?</h5>
-                            </button>
+
+                <div id="accordion" class="mt-4">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingOne">
+                            <div class="mb-0">
+                                <button class="btn btn-link text-dark" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <h5>1. Dimana lokasi Omid Health Style?</h5>
+                                </button>
+                            </div>
                         </div>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Kantor utama kami berada di Jl. Kemang Raya No. 69C (Persis di seberang KOI Kemang dan disamping iColor Kemang).
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <div class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                <h5>2. Apakah barang dapat dikirimkan keluar kota?</h5>
-                            </button>
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingTwo">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <h5>2. Apakah barang dapat dikirimkan keluar kota?</h5>
+                                </button>
+                            </h5>
                         </div>
-                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Semua produk dapat kami kirimkan ke luar kota kecuali buah-buahan segar, hummus, yogurt, sayur-sayuran, ice cream, keju (hanya pada kemasan asli bukan repackaging) dan makanan frozen lainnya, but we are working on it! Namun, kami dapat mengirimkan pesanan ke seluruh wilayah Indonesia (ada promo gratis ongkir khusus reseller).
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                <h5>3. Mendukung jasa pengiriman apa?</h5>
-                            </button>
-                        </h2>
-                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingThree">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <h5>3. Mendukung jasa pengiriman apa?</h5>
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Kami terutama mengirim melalui secara reguler Sicepat, JNE, J&T, Wahana, TIKI, Anter Aja, and ID Express.
-                                Untuk waktu pemesanan maksimal pukul 15.00 akan dikirim pada hari yang sama
+                                Untuk waktu pemesanan maksimal pukul 15.00 akan dikirim pada hari yang sama.
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingFour">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                <h5>4. Apakah barangnya ready?</h5>
-                            </button>
-                        </h2>
-                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingFour">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    <h5>4. Apakah barangnya ready?</h5>
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Yes! semua barang yang tersedia di etalase pasti ready stock ya, jika barang kosong akan segera kami informasikan ke buyer.
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingFive">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                <h5>5. Apakah Anda menawarkan harga grosir untuk restoran dan bisnis?</h5>
-                            </button>
-                        </h2>
-                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingFive">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    <h5>5. Apakah Anda menawarkan harga grosir untuk restoran dan bisnis?</h5>
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Iya! Kami terutama menawarkan akun grosir untuk bisnis yang ingin menjual kacang, dried fruits, atau produk yang tersedia pada etalase dapat anda pesan, Please submit a wholesale application here.
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingSix">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                <h5>6. Bagaimana untuk expired date?</h5>
-                            </button>
-                        </h2>
-                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingSix">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                    <h5>6. Bagaimana untuk expired date?</h5>
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Expired date kami selalu up to date, jadi sesuai dengan yang dideskripsi ya, jika ada pertanyaan lain silahkan hubungi customer services kami.
                             </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingSeven">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-                                <h5>7. Bagaimana cara pengembalian barang atau refund?</h5>
-                            </button>
-                        </h2>
-                        <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
+                    <div class="card">
+                        <div class="card-header bg-white" id="headingSeven">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                    <h5>7. Bagaimana cara pengembalian barang atau refund?</h5>
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+                            <div class="card-body text-muted">
                                 Silahkan hubungi admin untuk informasi permasalahan pada produk, jangan lupa sertakan video atau foto terkait dengan kondisi barangnya ya.
                             </div>
                         </div>

a6d9356b96f3853c0f3804fe565f5a30eb519b4b
wildanGhifari <wildghifari@gmail.com>
2021-03-19 09:25:49 +0700

    Membuat controller dan view untuk Halaman FAQs

--- application/views/layouts/app.php
+++ application/views/layouts/app.php
@@ -8,8 +8,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('/assets/libs/flickity/flickity.css') ?>">
 
-    <!-- Bootstrap core CSS -->
+    <!-- Bootstrap 4 core CSS -->
     <link href="<?= base_url('/assets/libs/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <!-- Bootstrap 5 core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <!-- Fontawesome CSS -->
     <link rel="stylesheet" href="<?= base_url('/assets/libs/fontawesome/css/all.min.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -35,6 +37,7 @@
     <!-- End of Footer -->
     <script src="<?= base_url('/assets/libs/jquery/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('/assets/libs/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="<?= base_url('/assets/libs/flickity/flickity.pkgd.min.js') ?>"></script>
     <script src="<?= base_url('/assets/js/app.js') ?>"></script>
     <script src="<?= base_url('/assets/js/ongkir.js') ?>"></script>
---
+++ application/views/pages/faqs/index.php
@@ -0,0 +1,96 @@
+<main role="main">
+    <div class="container-xl" style="padding: 5% 0;">
+        <div class="row">
+            <div class="col-md-10" style="margin:0 auto;">
+                <h3>Frequently asked questions</h3>
+                <div class="accordion mt-4" id="accordionExample">
+                    <div class="accordion-item">
+                        <div class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <h5>1. Dimana lokasi Omid Health Style?</h5>
+                            </button>
+                        </div>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Kantor utama kami berada di Jl. Kemang Raya No. 69C (Persis di seberang KOI Kemang dan disamping iColor Kemang).
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <div class="accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                <h5>2. Apakah barang dapat dikirimkan keluar kota?</h5>
+                            </button>
+                        </div>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Semua produk dapat kami kirimkan ke luar kota kecuali buah-buahan segar, hummus, yogurt, sayur-sayuran, ice cream, keju (hanya pada kemasan asli bukan repackaging) dan makanan frozen lainnya, but we are working on it! Namun, kami dapat mengirimkan pesanan ke seluruh wilayah Indonesia (ada promo gratis ongkir khusus reseller).
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                <h5>3. Mendukung jasa pengiriman apa?</h5>
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Kami terutama mengirim melalui secara reguler Sicepat, JNE, J&T, Wahana, TIKI, Anter Aja, and ID Express.
+                                Untuk waktu pemesanan maksimal pukul 15.00 akan dikirim pada hari yang sama
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                <h5>4. Apakah barangnya ready?</h5>
+                            </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Yes! semua barang yang tersedia di etalase pasti ready stock ya, jika barang kosong akan segera kami informasikan ke buyer.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <h5>5. Apakah Anda menawarkan harga grosir untuk restoran dan bisnis?</h5>
+                            </button>
+                        </h2>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Iya! Kami terutama menawarkan akun grosir untuk bisnis yang ingin menjual kacang, dried fruits, atau produk yang tersedia pada etalase dapat anda pesan, Please submit a wholesale application here.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                <h5>6. Bagaimana untuk expired date?</h5>
+                            </button>
+                        </h2>
+                        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Expired date kami selalu up to date, jadi sesuai dengan yang dideskripsi ya, jika ada pertanyaan lain silahkan hubungi customer services kami.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingSeven">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                                <h5>7. Bagaimana cara pengembalian barang atau refund?</h5>
+                            </button>
+                        </h2>
+                        <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                Silahkan hubungi admin untuk informasi permasalahan pada produk, jangan lupa sertakan video atau foto terkait dengan kondisi barangnya ya.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
